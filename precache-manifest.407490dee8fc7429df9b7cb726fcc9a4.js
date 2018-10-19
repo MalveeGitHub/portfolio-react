b6ccd71e7b9871588c84d01091aa9e41966763b3
alvee <you@example.com>
2018-10-19 12:25:47 -0400

    Updates

---
+++ precache-manifest.407490dee8fc7429df9b7cb726fcc9a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1212358ca090311f4fd1f27adf3cc4c2",
+    "url": "/portfolio-react/static/media/a.1212358c.jpg"
+  },
+  {
+    "revision": "9338e8a022e42c6bc02f",
+    "url": "/portfolio-react/static/js/runtime~main.9338e8a0.js"
+  },
+  {
+    "revision": "e05e5c3fbc88767c93d2",
+    "url": "/portfolio-react/static/js/main.e05e5c3f.chunk.js"
+  },
+  {
+    "revision": "95aab46e3a330b121512",
+    "url": "/portfolio-react/static/js/1.95aab46e.chunk.js"
+  },
+  {
+    "revision": "e05e5c3fbc88767c93d2",
+    "url": "/portfolio-react/static/css/main.e942615c.chunk.css"
+  },
+  {
+    "revision": "47a40800d37f53023776146ebe9f66a5",
+    "url": "/portfolio-react/index.html"
+  }
+];
